897f5cd0ee11e364ad2d14bc24bb15144012d9d8
fureweb-com <fureweb-com@gmail.com>
2019-10-02 16:54:03 +0900

    scss 처리를 위한 모듈 및 설정 추가
    - vscode에서 syntax highlight에 현재 문제가 있는 상태로 해결 필요

---
+++ src/routes/test.svelte
@@ -0,0 +1,25 @@
+<script>
+  export let pageName = 'test'
+</script>
+
+<style lang="scss">
+  $color: red;
+
+  h1 {
+    color: $color;
+  }
+
+  div {
+    background: blue;
+
+    > p {
+      color: #fff;
+    }
+  }
+</style>
+
+<h1>page: {pageName}!</h1>
+
+<div>
+  <p>SASS is working!</p>
+</div>
